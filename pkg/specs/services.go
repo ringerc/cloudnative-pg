24b65954bad7d26c655788754bef6b5c69f3f8e8
Armando Ruocco <armando.ruocco@enterprisedb.com>
2024-06-18 13:54:38 +0200

    chore: deprecate `role` label
    Closes #4894
    
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- pkg/specs/services.go
+++ pkg/specs/services.go
@@ -85,9 +85,8 @@ func CreateClusterReadOnlyService(cluster apiv1.Cluster) *corev1.Service {
 			Type:  corev1.ServiceTypeClusterIP,
 			Ports: buildInstanceServicePorts(),
 			Selector: map[string]string{
-				utils.ClusterLabelName: cluster.Name,
-				// TODO: eventually migrate to the new label
-				utils.ClusterRoleLabelName: ClusterRoleLabelReplica,
+				utils.ClusterLabelName:             cluster.Name,
+				utils.ClusterInstanceRoleLabelName: ClusterRoleLabelReplica,
 			},
 		},
 	}
@@ -104,8 +103,8 @@ func CreateClusterReadWriteService(cluster apiv1.Cluster) *corev1.Service {
 			Type:  corev1.ServiceTypeClusterIP,
 			Ports: buildInstanceServicePorts(),
 			Selector: map[string]string{
-				utils.ClusterLabelName:     cluster.Name,
-				utils.ClusterRoleLabelName: ClusterRoleLabelPrimary,
+				utils.ClusterLabelName:             cluster.Name,
+				utils.ClusterInstanceRoleLabelName: ClusterRoleLabelPrimary,
 			},
 		},
 	}
